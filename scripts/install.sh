882240ab13dbf418cfb05df82af869d5eccd9172
Antonio Dell'Annunziata <antonio.dell@gmx.de>
2021-01-30 23:20:36 +0100

    Fix error in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@ then
     echo "Install deno..."
     curl -fsSL https://deno.land/x/install/install.sh | sh
     echo 'export DENO_INSTALL=\"$deno_install\"' >> ~/.bashrc 
-    echo  'export PATH=\"\$DENO_INSTALL/bin:\$PATH\"' >> ~/.bashrc 
 fi
 
 if ! command -v npm
